cc10de9ee32204f642941af8f899ca608f6f70e9
Hanno Georg <hanno@georg.one>
2026-06-12 19:17:10 +0200

    Refactor logging setup and improve variable declarations for clarity in modbusAccess function

--- huaweisolar.py
+++ huaweisolar.py
@@ -5,42 +5,150 @@ import paho.mqtt.client
 import os
 
 import logging
-FORMAT = ('%(asctime)-15s %(threadName)-15s '
-          '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+
+FORMAT = (
+    "%(asctime)-15s %(threadName)-15s "
+    "%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s"
+)
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
-log_level = os.getenv('LOG_LEVEL', 'INFO')
+log_level = os.getenv("LOG_LEVEL", "INFO")
 log.setLevel(log_level)
 
-inverter_ip = os.getenv('INVERTER_IP', '192.168.0.137')
-mqtt_host = os.getenv('MQTT_HOST', '192.168.0.10')
+inverter_ip = os.getenv("INVERTER_IP", "192.168.0.137")
+mqtt_host = os.getenv("MQTT_HOST", "192.168.0.10")
 
 inverter = huawei_solar.HuaweiSolar(inverter_ip, port=502, slave=1)
 inverter._slave = 1
 inverter.wait = 1
 
-#vars = ['state_1','state_2', 'state_3', 'alarm_1', 'alarm_2', 'alarm_3', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage', 'grid_current', 'day_active_power_peak', 'active_power', 'reactive_power', 'grid_frequency', 'efficiency', 'internal_temperature', 'insulation_resistance', 'device_status', 'fault_code', 'startup_time', 'shutdown_time', 'accumulated_yield_energy', 'daily_yield_energy', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power', 'grid_exported_energy', 'grid_accumulated_energy']
+# vars = ['state_1','state_2', 'state_3', 'alarm_1', 'alarm_2', 'alarm_3', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage', 'grid_current', 'day_active_power_peak', 'active_power', 'reactive_power', 'grid_frequency', 'efficiency', 'internal_temperature', 'insulation_resistance', 'device_status', 'fault_code', 'startup_time', 'shutdown_time', 'accumulated_yield_energy', 'daily_yield_energy', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power', 'grid_exported_energy', 'grid_accumulated_energy']
 
-#vars = ['alarm_1', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage', 'grid_current', 'day_active_power_peak', 'active_power', 'reactive_power', 'grid_frequency', 'efficiency', 'internal_temperature', 'insulation_resistance', 'device_status', 'fault_code', 'startup_time', 'shutdown_time', 'accumulated_yield_energy', 'daily_yield_energy', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power', 'grid_exported_energy', 'grid_accumulated_energy']
+# vars = ['alarm_1', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage', 'grid_current', 'day_active_power_peak', 'active_power', 'reactive_power', 'grid_frequency', 'efficiency', 'internal_temperature', 'insulation_resistance', 'device_status', 'fault_code', 'startup_time', 'shutdown_time', 'accumulated_yield_energy', 'daily_yield_energy', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power', 'grid_exported_energy', 'grid_accumulated_energy']
 
 
 def modbusAccess():
 
-    vars_inmediate = ['pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage', 'grid_current', 'active_power', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power', 'storage_unit_1_charge_discharge_power', 'storage_state_of_capacity', 'storage_rated_capacity', 'storage_running_status', 'storage_charge_discharge_power', 'storage_bus_voltage', 'storage_bus_current']
+    vars_inmediate = [
+        "pv_01_voltage",
+        "pv_01_current",
+        "pv_02_voltage",
+        "pv_02_current",
+        "input_power",
+        "grid_voltage",
+        "grid_current",
+        "active_power",
+        "grid_A_voltage",
+        "active_grid_A_current",
+        "power_meter_active_power",
+        "storage_unit_1_charge_discharge_power",
+        "storage_state_of_capacity",
+        "storage_rated_capacity",
+        "storage_running_status",
+        "storage_charge_discharge_power",
+        "storage_bus_voltage",
+        "storage_bus_current",
+    ]
 
-    vars = ['day_active_power_peak', 'efficiency', 'internal_temperature', 'insulation_resistance', 'device_status', 'fault_code', 'accumulated_yield_energy', 'daily_yield_energy', 'grid_exported_energy', 'grid_accumulated_energy', 'storage_current_day_charge_capacity', 'storage_current_day_discharge_capacity']
+    vars = [
+        "day_active_power_peak",
+        "efficiency",
+        "internal_temperature",
+        "insulation_resistance",
+        "device_status",
+        "fault_code",
+        "accumulated_yield_energy",
+        "daily_yield_energy",
+        "grid_exported_energy",
+        "grid_accumulated_energy",
+        "storage_current_day_charge_capacity",
+        "storage_current_day_discharge_capacity",
+    ]
 
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/pv_01_voltage/config", payload= '{"uniq_id": "pv_01_voltage", "name": "Huawei 01 Voltage", "dev_cla": "power", "stat_t": "emon/NodeHuawei/pv_01_voltage", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/pv_02_voltage/config", payload= '{"uniq_id": "pv_02_voltage", "name": "Huawei 02 Voltage", "dev_cla": "power", "stat_t": "emon/NodeHuawei/pv_02_voltage", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/pv_01_current/config", payload= '{"uniq_id": "pv_01_current", "name": "Huawei 01 Current", "dev_cla": "current", "stat_t": "emon/NodeHuawei/pv_01_current", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/pv_02_current/config", payload= '{"uniq_id": "pv_02_current", "name": "Huawei 02 Current", "dev_cla": "current", "stat_t": "emon/NodeHuawei/pv_02_current", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/input_power/config", payload= '{"uniq_id": "input_power", "name": "Huawei Input power", "dev_cla": "power", "stat_t": "emon/NodeHuawei/input_power", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/grid_voltage/config", payload= '{"uniq_id": "grid_voltage", "name": "Huawei Grid Voltage", "dev_cla": "power", "stat_t": "emon/NodeHuawei/grid_voltage", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/grid_current/config", payload= '{"uniq_id": "grid_current", "name": "Huawei Grid Current", "dev_cla": "current", "stat_t": "emon/NodeHuawei/grid_current", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/accumulated_yield_energy/config", payload= '{"uniq_id": "accumulated_yield_energy", "name": "Huawei Total Yield", "dev_cla": "energy", "stat_t": "emon/NodeHuawei/accumulated_yield_energy", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/daily_yield_energy/config", payload= '{"uniq_id": "daily_yield_energy", "name": "Huawei Daily Yield", "dev_cla": "energy", "stat_t": "emon/NodeHuawei/daily_yield_energy", "unit_of_meas": "' + huawei_solar.REGISTERS["pv_01_voltage"].unit + '"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/device_status/config", payload= '{"uniq_id": "device_status", "name": "Huawei device status", "stat_t": "emon/NodeHuawei/device_status"}', qos=1, retain=False)
-    clientMQTT.publish(topic="homeassistant/sensor/HuaweiInverter/fault_code/config", payload= '{"uniq_id": "fault_code", "name": "Huawei fault code", "stat_t": "emon/NodeHuawei/fault_code"}', qos=1, retain=False)
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/pv_01_voltage/config",
+        payload='{"uniq_id": "pv_01_voltage", "name": "Huawei 01 Voltage", "dev_cla": "power", "stat_t": "emon/NodeHuawei/pv_01_voltage", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/pv_02_voltage/config",
+        payload='{"uniq_id": "pv_02_voltage", "name": "Huawei 02 Voltage", "dev_cla": "power", "stat_t": "emon/NodeHuawei/pv_02_voltage", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/pv_01_current/config",
+        payload='{"uniq_id": "pv_01_current", "name": "Huawei 01 Current", "dev_cla": "current", "stat_t": "emon/NodeHuawei/pv_01_current", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/pv_02_current/config",
+        payload='{"uniq_id": "pv_02_current", "name": "Huawei 02 Current", "dev_cla": "current", "stat_t": "emon/NodeHuawei/pv_02_current", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/input_power/config",
+        payload='{"uniq_id": "input_power", "name": "Huawei Input power", "dev_cla": "power", "stat_t": "emon/NodeHuawei/input_power", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/grid_voltage/config",
+        payload='{"uniq_id": "grid_voltage", "name": "Huawei Grid Voltage", "dev_cla": "power", "stat_t": "emon/NodeHuawei/grid_voltage", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/grid_current/config",
+        payload='{"uniq_id": "grid_current", "name": "Huawei Grid Current", "dev_cla": "current", "stat_t": "emon/NodeHuawei/grid_current", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/accumulated_yield_energy/config",
+        payload='{"uniq_id": "accumulated_yield_energy", "name": "Huawei Total Yield", "dev_cla": "energy", "stat_t": "emon/NodeHuawei/accumulated_yield_energy", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/daily_yield_energy/config",
+        payload='{"uniq_id": "daily_yield_energy", "name": "Huawei Daily Yield", "dev_cla": "energy", "stat_t": "emon/NodeHuawei/daily_yield_energy", "unit_of_meas": "'
+        + huawei_solar.REGISTERS["pv_01_voltage"].unit
+        + '"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/device_status/config",
+        payload='{"uniq_id": "device_status", "name": "Huawei device status", "stat_t": "emon/NodeHuawei/device_status"}',
+        qos=1,
+        retain=False,
+    )
+    clientMQTT.publish(
+        topic="homeassistant/sensor/HuaweiInverter/fault_code/config",
+        payload='{"uniq_id": "fault_code", "name": "Huawei fault code", "stat_t": "emon/NodeHuawei/fault_code"}',
+        qos=1,
+        retain=False,
+    )
 
     cont = 0
     while True:
@@ -52,11 +160,16 @@ def modbusAccess():
                 log.debug(mid)
                 log.debug("---")
 
-                clientMQTT.publish(topic="emon/NodeHuawei/"+i, payload= str(mid.value), qos=1, retain=False)
+                clientMQTT.publish(
+                    topic="emon/NodeHuawei/" + i,
+                    payload=str(mid.value),
+                    qos=1,
+                    retain=False,
+                )
             except:
                 pass
 
-        if(cont > 5):
+        if cont > 5:
             for i in vars:
                 try:
                     mid = inverter.get(i)
@@ -64,37 +177,39 @@ def modbusAccess():
                     log.debug(mid)
                     log.debug("---")
 
-                    clientMQTT.publish(topic="emon/NodeHuawei/"+i, payload= str(mid.value), qos=1, retain=False)
+                    clientMQTT.publish(
+                        topic="emon/NodeHuawei/" + i,
+                        payload=str(mid.value),
+                        qos=1,
+                        retain=False,
+                    )
                 except:
                     pass
 
             cont = 0
 
-        cont = cont + 1   
+        cont = cont + 1
         time.sleep(1)
 
 
-
-
-
 def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        client.connected_flag=True #set flag
+    if rc == 0:
+        client.connected_flag = True  # set flag
         log.info("MQTT OK!")
     else:
-        log.info("MQTT FAILURE. ERROR CODE: %s",rc)
+        log.info("MQTT FAILURE. ERROR CODE: %s", rc)
 
 
-paho.mqtt.client.Client.connected_flag=False#create flag in class
+paho.mqtt.client.Client.connected_flag = False  # create flag in class
 broker_port = 1883
 
 clientMQTT = paho.mqtt.client.Client()
-clientMQTT.on_connect=on_connect #bind call back function
+clientMQTT.on_connect = on_connect  # bind call back function
 clientMQTT.loop_start()
-log.info("Connecting to MQTT broker: %s ",mqtt_host)
-clientMQTT.username_pw_set(username="",password="")
-clientMQTT.connect(mqtt_host, broker_port) #connect to broker
-while not clientMQTT.connected_flag: #wait in loop
+log.info("Connecting to MQTT broker: %s ", mqtt_host)
+clientMQTT.username_pw_set(username="", password="")
+clientMQTT.connect(mqtt_host, broker_port)  # connect to broker
+while not clientMQTT.connected_flag:  # wait in loop
     log.info("...")
 time.sleep(1)
 log.info("START MODBUS...")
